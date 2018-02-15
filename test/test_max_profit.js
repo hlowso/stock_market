1bc2d38f1c91fdf3a39cf5d40b009fc77b712dbe
hlowso <h.canderleigh@gmail.com>
2018-02-15 23:07:39 +0000

    changed definition of successful run of maxProfit in test script

--- test/test_max_profit.js
+++ test/test_max_profit.js
@@ -2,7 +2,7 @@ var assert = require("chai").assert;
 var maxProfit = require("../lib/max_profit.js");
 
 describe("check", function() {
-  it("should return true if a maxProfit successfully determines the maximum difference between any two elements in an array of numbers", function() {
+  it("should return true if a maxProfit successfully determines the maximum difference between any two elements a and b in an array of numbers, where the index of b is greater than that of a", function() {
     var stock_prices = [45, 24, 35, 31, 40, 38, 11];
     assert.isTrue(maxProfit(stock_prices) === 16);
   });
